b36f71e5f08df6cf3d8bc4e423dd5f5c57cd24f7
Saravana Kumar J <saravana.k@unislink.com>
2018-06-28 13:04:10 +0530

    result importer - description

--- lib/health-data-standards/import/ccda/result_importer.rb
+++ lib/health-data-standards/import/ccda/result_importer.rb
@@ -3,7 +3,7 @@ module HealthDataStandards
     module CCDA
       class ResultImporter < CDA::ResultImporter
         def initialize
-          super(CDA::EntryFinder.new("//cda:observation[@duplicate='false'][cda:templateId/@root='2.16.840.1.113883.10.20.22.4.2']"))
+          super(CDA::EntryFinder.new("//cda:observation[@duplicate='false'][cda:templateId/@root='2.16.840.1.113883.10.20.22.4.2']"))          
         end
 
         def create_entries(doc, nrh = NarrativeReferenceHandler.new)
@@ -36,6 +36,13 @@ module HealthDataStandards
             value_element = parent_element.xpath("//cda:content[@ID='#{referenceKey}']/../../cda:td[3]")
             extract_value(parent_element, value_element.first, entry) if value_element.first
           end
+        end
+
+        def extract_description(parent_element, entry, nrh)
+          super
+          
+          currentElementText = parent_element.at_xpath("./cda:text").try("text")
+          entry.description = currentElementText if currentElementText
         end
 
         private
